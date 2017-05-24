5b2390141a8c2a50266d7516083275f7bfe8a4cd
berny11 <bernyalem@gmail.com>
2017-05-24 00:32:54 -0700

    Update client.java

--- src/client.java
+++ src/client.java
@@ -1,13 +1,5 @@
 
-import java.io.DataOutputStream;
-import java.io.OutputStream;
-import java.net.Socket;
 
-public class client {
-    int abc;
-    
-    String a="hello world";
-    public static void main(String args[]) throws Exception {
 
         Socket sock = new Socket("127.0.0.1", 5000);
         String message1 = "Accept best wishes server";
