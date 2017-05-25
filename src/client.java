51704a17201c60f39c1bdb3f96081f796e32b0be
henok <henok@10.2.80.97>
2017-05-24 22:22:27 -0700

    comitment

---
+++ src/client.java
@@ -0,0 +1,22 @@
+
+import java.io.DataOutputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class client {
+
+    public static void main(String args[]) throws Exception {
+
+        Socket sock = new Socket("127.0.0.1", 5000);
+        String message1 = "Accept best wishes server";
+
+        OutputStream ostream = sock.getOutputStream();
+        DataOutputStream dos = new DataOutputStream(ostream);
+
+        dos.writeBytes(message1);
+        dos.close();
+        ostream.close();
+        sock.close();
+    }
+
+}
